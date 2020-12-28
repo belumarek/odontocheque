4fbbe92f03af0f939083689af337c4c2ce2fbace
pablo malczewski <malczewskipablo@gmail.com>
2020-12-28 16:41:50 -0300

    adding procinces

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PROVINCES = %w[CABA Buenos Aires Catamarca Chaco Chubut Córdoba Corrientes Entre Ríos Formosa Jujuy La Pampa La Rioja Mendoza Misiones Neuquén Río Negro Salta San Juan Santa Cruz Santa Fe Santiago del Estero Tierra del Fuego Tucumán]
 end
