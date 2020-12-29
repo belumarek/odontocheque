fcceafab8f900f9ae7a23c3bb66e75e0873df596
pablo malczewski <malczewskipablo@gmail.com>
2020-12-28 21:44:05 -0300

    adding validates

--- app/models/registration.rb
+++
@@ -1,9 +0,0 @@
-class Registration < ApplicationRecord
-  attr :province
-  attr :number
-  validate :number, uniqueness: { scope: :kind }
-  validate :registration_uniqueness
-
-
-PROVINCES = %w[Buenos Aires CABA Catamarca Chaco Chubut Córdoba  Corrientes Entre Ríos  Formosa Jujuy La Pampa La Rioja Mendoza: Mendoza Misiones Neuquén Río Negro Salta San Juan San Luis Santa Cruz Santa Fe Santiago del Estero Tierra del Fuego Tucumán]
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PROVINCES = ['CABA', 'Buenos Aires', 'Catamarca', 'Chaco', 'Chubut', 'Córdoba', 'Corrientes', 'Entre Ríos', 'Formosa', 'Jujuy', 'La Pampa' 'La Rioja', 'Mendoza', 'Misiones', 'Neuquén', 'Río Negro', 'Salta', 'San Juan', 'Santa Cruz', 'Santa Fe', 'Santiago del Estero', 'Tierra del Fuego', 'Tucumán']
 
-  PROVINCES = %w[CABA Buenos Aires Catamarca Chaco Chubut Córdoba Corrientes Entre Ríos Formosa Jujuy La Pampa La Rioja Mendoza Misiones Neuquén Río Negro Salta San Juan Santa Cruz Santa Fe Santiago del Estero Tierra del Fuego Tucumán]
+  validates :province, inclusion: { in: PROVINCES }
+  validates :registration_number, length: { is: 6 }, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :first_name, length: { in: 2..20 }, allow_blank: true
+  validates :last_name, presence: true
+  validates :last_name, length: { in: 2..20 }, allow_blank: true
 end
